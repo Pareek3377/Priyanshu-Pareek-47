2cd6dabf5eb9b2af3477f8501bb4f54f4a5100ae
Pareek3377 <120202241+Pareek3377@users.noreply.github.com>
2024-05-20 11:30:52 +0530

    Update Railway.sql

--- Railway.sql
+++ Railway.sql
@@ -1,20 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Oct 13, 2016 at 03:20 PM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `railway`
